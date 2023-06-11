20f2d0b2567e3c6ddee153fdf47da260b8ee8833
AleksTimofeev <timoha144a@gmail.com>
2023-06-11 23:04:39 +0300

    change typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,14 +8,20 @@ import { User } from "./users/users.entity";
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: "postgres",
-      host: "localhost",
-      port: 5432,
-      username: "postgres",
-      password: "Sasha26031003",
-      database: "nest-test-project",
-      entities: [User],
-      synchronize: true
+      // type: "postgres",
+      // host: "localhost",
+      // port: 5432,
+      // username: "postgres",
+      // password: "Sasha26031003",
+      // database: "nest-test-project",
+      // entities: [User],
+      // synchronize: true
+      "name": "default",
+      "type": "postgres",
+      "url": 'postgres://sncqcowa:YKQIAJgM9_KuAu39WvTJVznjbJyyjcm2@dumbo.db.elephantsql.com/sncqcowa',
+      "synchronize": true,
+      "logging": true,
+      entities: [User]
     }),
     UsersModule
   ],
