bc5698dee1eb54cad4daa19812e4658d74a7e3dd
AleksTimofeev <timoha144a@gmail.com>
2023-06-11 22:52:15 +0300

    ...

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { User } from "./users/users.entity";
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: "postgres",
+      password: "Sasha26031003",
+      database: "nest-test-project",
+      entities: [User],
+      synchronize: true
+    }),
+    UsersModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
