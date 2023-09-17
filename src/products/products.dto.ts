1c6c04572f40283f4d3b49d1e97b455a2393bbfc
AleksTimofeev <timoha144a@gmail.com>
2023-09-17 23:41:35 +0300

    refactor update product

--- src/products/products.dto.ts
+++ src/products/products.dto.ts
@@ -1,3 +1,4 @@
+import { IsNotEmpty, IsString } from "class-validator";
 
 
 export class CreateProductsDto {
@@ -10,12 +11,20 @@ export class CreateProductsDto {
   readonly imgUrlSmall: string
 }
 export class ChangeProductDto {
+  @IsString()
   readonly id: string
+  @IsString()
   readonly title: string
+  @IsString()
   readonly description: string
+  @IsString()
   readonly minDescription: string
+  @IsString()
   readonly rating: string
+  @IsString()
   readonly price: string
+  @IsString()
   readonly imgUrlLarge: string
+  @IsString()
   readonly imgUrlSmall: string
 }
